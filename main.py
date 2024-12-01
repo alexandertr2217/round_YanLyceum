351a02f0a5dbd5c38e4dc30e394a7f7853380d0b
Alex <alekstrepacev@gmail.com>
2024-12-01 20:56:30 +0300

    add random color and load ui from py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sys
 from random import randrange
 
 from PyQt6 import uic
+from ui import Ui_MainWindow
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
-class Osnova(QMainWindow):
+class Osnova(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.pushButton.clicked.connect(self.naris)
 
@@ -25,8 +26,8 @@ class Osnova(QMainWindow):
         self.update()
 
     def ris(self, qpai):
-        qpai.setBrush(QColor(255, 255, 2))
         for i in range(randrange(1, 10)):
+            qpai.setBrush(QColor(randrange(0, 256), randrange(0, 256), randrange(0, 256)))
             rad = randrange(1, 150)
             qpai.drawEllipse(randrange(1, 700), randrange(1, 600), rad, rad)
 
